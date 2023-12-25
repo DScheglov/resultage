a266b27f0d178c9a472758a6e1e7cd8ae8aadff0
Dmitry Scheglov <dmitry.scheglov@gmail.com>
2023-12-25 16:51:42 +0100

    [dsch] updating the check version script

--- version.sh
+++ version.sh
@@ -1,3 +1,8 @@
-PACKAGE=$(npm pgk get name)@$(npm pgk get version)
+PACKAGE=$(npm pkg get name | xargs)@$(npm pkg get version | xargs)
 VERSION=$(npm view $PACKAGE version || echo "")
-if [[ -n $VERSION ]]; then echo "Couldn't publish over existing version."; exit 1; fi
+
+if [[ -n $VERSION ]]; then 
+  echo "Couldn't publish over existing version."; exit 1; 
+else 
+  echo "Publishing $PACKAGE";
+fi
