9a94460a39e1d42556d51476d7b673f9e7ec0fa9
Dmitry Scheglov <dmitry.scheglov@gmail.com>
2024-09-22 16:46:17 +0200

    [dsch] minor pipe improvement and its tests refactoring

--- src/fn/pipe.test.ts
+++ src/fn/pipe.test.ts
@@ -6,14 +6,215 @@ describe('pipe', () => {
     expect(pipe(1)).toBe(1);
   });
 
-  it.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] as const)(
-    'works with %i functions',
-    (n) => {
-      const fns = Array.from({ length: n }, () => (x: number) => x + 1) as [
-        (x: number) => number,
-      ];
-      const result = pipe(1, ...fns);
-      expect(result).toBe(n + 1);
-    },
-  );
+  it('works with 1 function', () => {
+    expect(pipe(1, (x: number) => x + 1)).toBe(2);
+  });
+
+  it('works with 2 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(3);
+  });
+
+  it('works with 3 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(4);
+  });
+
+  it('works with 4 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(5);
+  });
+
+  it('works with 5 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(6);
+  });
+
+  it('works with 6 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(7);
+  });
+
+  it('works with 7 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(8);
+  });
+
+  it('works with 8 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(9);
+  });
+
+  it('works with 9 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(10);
+  });
+
+  it('works with 10 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(11);
+  });
+
+  it('works with 11 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(12);
+  });
+
+  it('works with 12 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(13);
+  });
+
+  it('works with 13 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(14);
+  });
+
+  it('works with 14 functions', () => {
+    expect(
+      pipe(
+        1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+        (x: number) => x + 1,
+      ),
+    ).toBe(15);
+  });
 });
